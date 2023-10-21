6d43d9942d89a1e82ef98ba680cfdd3d006bae0f
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-21 11:16:49 +0800

    Update gen.sh

--- script/gen.sh
+++ script/gen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+git clone --depth=1 https://https://github.com/StaZhu/enable-chromium-hevc-hardware-decoding.git $HOME/hevc
 cd ~/chromium/src
 mkdir -p out/Release64/
 ls
-cd /third_party/ffmpeg
+cd third_party/ffmpeg
 ls
-git am /path/to/add-hevc-ffmpeg-decoder-parser.patch
+git am $HOME/hevc/add-hevc-ffmpeg-decoder-parser.patch
 cd ~/chromium/src
-cp -R /path/to/widevine/* third_party/widevine/cdm
+cp -R $HOME/hevc/widevine/* third_party/widevine/cdm
 gn gen out/Release64 --args="is_component_build = false is_official_build = true is_debug = false ffmpeg_branding = \"Chrome\" target_cpu = \"x64\" proprietary_codecs = true media_use_ffmpeg = true enable_widevine = true bundle_widevine_cdm = true enable_media_foundation_widevine_cdm = true"
