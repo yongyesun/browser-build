45cce674498f2ef2f64715055b56688712be9767
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-21 10:06:01 +0800

    Update build_step.sh

--- script/build_step.sh
+++ script/build_step.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-test -f "$FLAG_STOP" && echo skip && exit 0 || echo ninja
+test -f "$FLAG_STOP" && echo skip && exit 0 || echo ninja start
 cd ~/chromium/src
 autoninja -C out/Release64 chrome || ( test $? == 143 && echo ninja canceld )
 git config core.ignorecase false
