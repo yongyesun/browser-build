9f20357974a64ef56d4b71a2e541872dfeddf772
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-21 11:18:35 +0800

    Update download_src.sh

--- script/download_src.sh
+++ script/download_src.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install -y python ccache
-mkdir ~/chromium && cd ~/chromium
+mkdir $ROOT/chromium && cd $ROOT/chromium
 fetch --nohooks --no-history chromium
 cd src
 ./build/install-build-deps.sh
