487e67e7ecb646a691c7fd5da7843c1e45f38bc1
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-21 18:20:41 +0800

    Update gen.sh

--- script/gen.sh
+++ script/gen.sh
@@ -7,4 +7,4 @@ ls
 git am $ROOT/hevc/add-hevc-ffmpeg-decoder-parser.patch
 cd $ROOT/chromium/src
 cp -R $ROOT/hevc/widevine/* third_party/widevine/cdm
-gn gen out/Release64 --args="is_component_build = false is_official_build = true is_debug = false ffmpeg_branding = \"Chrome\" target_cpu = \"x64\" proprietary_codecs = true media_use_ffmpeg = true enable_widevine = true bundle_widevine_cdm = true enable_media_foundation_widevine_cdm = true"
+gn gen out/Release64 --args="is_component_build = false is_official_build = true is_debug = false ffmpeg_branding = \"Chrome\" target_cpu = \"x64\" proprietary_codecs = true media_use_ffmpeg = true"
