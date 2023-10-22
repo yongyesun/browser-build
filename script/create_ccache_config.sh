e895168815df488f11c017f6e4a72006523c4980
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-22 23:22:45 +0800

    Update create_ccache_config.sh

--- script/create_ccache_config.sh
+++ script/create_ccache_config.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-mkdir -p $HOME/.ccache/
-echo 'compiler_check = none' >> $HOME/.ccache/ccache.conf
-echo "stats = false" >> $HOME/.ccache/ccache.conf
-echo 'max_size = 20G' >> $HOME/.ccache/ccache.conf
-echo "base_dir = $HOME" >> $HOME/.ccache/ccache.conf
-echo "cache_dir = $HOME/cache" >> $HOME/.ccache/ccache.conf
-echo "hash_dir = false" >> $HOME/.ccache/ccache.conf
-git clone --depth=1 https://github.com/StaZhu/enable-chromium-hevc-hardware-decoding.git $ROOT/hevc
+mkdir -p $ROOT/.ccache/
+echo 'compiler_check = none' >> $ROOT/.ccache/ccache.conf
+echo "stats = false" >> $ROOT/.ccache/ccache.conf
+echo 'max_size = 20G' >> $ROOT/.ccache/ccache.conf
+echo "base_dir = $ROOT" >> $ROOT/.ccache/ccache.conf
+echo "cache_dir = $ROOT/cache" >> $ROOT/.ccache/ccache.conf
+echo "hash_dir = false" >> $ROOT/.ccache/ccache.conf
+
