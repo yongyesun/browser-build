216514982087f9936e804b36396cd788210c2abd
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-22 10:12:19 +0800

    Update gen.sh

--- script/gen.sh
+++ script/gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd $ROOT/chromium/src
+cd $ROOT/chromium
 mkdir -p out/Release64/
 gn gen out/Release64 --args="is_component_build = false is_official_build = true is_debug = false ffmpeg_branding = \"Chrome\" target_cpu = \"x64\" proprietary_codecs = true media_use_ffmpeg = true enable_widevine = true bundle_widevine_cdm = true enable_media_foundation_widevine_cdm = true enable_nacl=false blink_symbol_level=0 v8_symbol_level=0"
