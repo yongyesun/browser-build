0a7c480334c60894804e864f0d3fbcbed4fd2a7f
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-22 22:27:27 +0800

    Update build_step.sh

--- script/build_step.sh
+++ script/build_step.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 test -f "$FLAG_STOP" && echo skip && exit 0 || echo ninja start
-cp ccache /usr/local/bin/
+cp /usr/bin/ccache /usr/local/bin/
 ln -s ccache /usr/local/bin/gcc
 ln -s ccache /usr/local/bin/g++
 cd $ROOT/chromium/src
