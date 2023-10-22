972b073f717c44c23d4f2b0afdac4e69ddff7d2c
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-23 06:57:04 +0800

    Update download_src.sh

--- script/download_src.sh
+++ script/download_src.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y ccache
 git clone --depth=1 https://github.com/StaZhu/enable-chromium-hevc-hardware-decoding.git $ROOT/hevc
 mkdir $ROOT/chromium && cd $ROOT/chromium
 fetch --nohooks --no-history chromium
-sed -i 's/"checkout_pgo_profiles": False/"checkout_pgo_profiles": True/g .gclient
+sed -i 's/"checkout_pgo_profiles": False/"checkout_pgo_profiles": True/g' .gclient
 cd src
 cd third_party/ffmpeg
 git am $ROOT/hevc/add-hevc-ffmpeg-decoder-parser.patch
