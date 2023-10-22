6ea517dab729a53e79e5cf8cebda99cd83a7626b
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-22 22:02:31 +0800

    Update build_step.sh

--- script/build_step.sh
+++ script/build_step.sh
@@ -3,5 +3,5 @@ test -f "$FLAG_STOP" && echo skip && exit 0 || echo ninja start
 cp ccache /usr/local/bin/
 ln -s ccache /usr/local/bin/gcc
 ln -s ccache /usr/local/bin/g++
-cd $ROOT/chromium
+cd $ROOT/chromium/src
 autoninja -C out/Release64 chrome || ( test $? == 143 && echo ninja canceld )
