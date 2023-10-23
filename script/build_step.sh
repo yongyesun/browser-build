b5028101bdb435a640f9ce5466500c72b4782bd6
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-23 21:35:13 +0800

    Update build_step.sh

--- script/build_step.sh
+++ script/build_step.sh
@@ -5,10 +5,14 @@ ln -s ccache /usr/local/bin/gcc
 ln -s ccache /usr/local/bin/g++
 cd $ROOT/chromium/src
 autoninja -C out/Release64 chrome || ( test $? == 143 && echo ninja canceld )
-git config core.ignorecase false
-git config --local user.email "actions@github.com"
-git config --local user.name "GitHub Actions"
-git add "$ROOT/cache"
-git commit -m "$(date '+%Y-%m-%d %H:%M:%S')更新" || echo no change,
-git pull origin main
-git push origin main
+COUNT=$(ps -C ninja --no-header |wc -l)
+if [ $COUNT -eq 0 ]
+then
+  git config core.ignorecase false
+  git config --local user.email "actions@github.com"
+  git config --local user.name "GitHub Actions"
+  git add "$ROOT/cache"
+  git commit -m "$(date '+%Y-%m-%d %H:%M:%S')更新" || echo no change,
+  git pull origin main
+  git push origin main
+fi
