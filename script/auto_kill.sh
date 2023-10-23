94e49335298c52864bdde4af2f31614fc0ece8ba
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-23 17:50:54 +0800

    Update auto_kill.sh

--- script/auto_kill.sh
+++ script/auto_kill.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 sleep $(expr 60 \* 60 \* 1 + ${START_TIME}  - $(date "+%s")) && touch "$FLAG_STOP" && ( killall ninja-linux64 || true ) || ( test $? == 143 && echo sleep canceld ) &
+ps aux | grep 'sleep'
