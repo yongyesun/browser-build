cfb14e526294b3e7da94212f0b6a4866272acee7
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-24 17:39:30 +0800

    Update create_ccache_config.sh

--- script/create_ccache_config.sh
+++ script/create_ccache_config.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+sudo apt-get install -y ccache
 mkdir -p $HOME/.ccache/
+mkdir -p $ROOT/cache/
 echo 'compiler_check = none' >> $HOME/.ccache/ccache.conf
 echo "stats = false" >> $HOME/.ccache/ccache.conf
 echo 'max_size = 20G' >> $HOME/.ccache/ccache.conf
@@ -7,4 +9,3 @@ echo "base_dir = $ROOT" >> $HOME/.ccache/ccache.conf
 echo "cache_dir = $ROOT/cache" >> $HOME/.ccache/ccache.conf
 echo "hash_dir = false" >> $HOME/.ccache/ccache.conf
 #git clone --depth=1 https://github.com/AoEiuV020/chromium-ccache-arm64.git $ROOT/cache
-mkdir -p $ROOT/cache/
