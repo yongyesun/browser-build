911f015f298c0d82a86bb599b3dc9fcd68d72c7c
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-24 14:21:15 +0800

    Update build_step.sh

--- script/build_step.sh
+++ script/build_step.sh
@@ -8,12 +8,12 @@ autoninja -C out/Release64 chrome || echo ninja canceld code $?
 COUNT=$(ps -C sleep --no-header |wc -l)
 if [ $COUNT -eq 0 ]
 then
-  cd $ROOT/ccache && ls -a && echo "/nls ccache"
+  cd $ROOT/cache && ls -a && echo "/nls ccache"
   cd $ROOT
   git config core.ignorecase false
   git config --local user.email "actions@github.com"
   git config --local user.name "GitHub Actions"
-  git add "./ccache"
+  git add "./cache"
   git commit -m "$(date '+%Y-%m-%d %H:%M:%S')更新" || echo no change,
   git pull origin main
   git push origin main
