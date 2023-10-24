d29174dd44a7891b834bc0c8a2321278aeb45530
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-24 15:40:33 +0800

    Update create_ccache_config.sh

--- script/create_ccache_config.sh
+++ script/create_ccache_config.sh
@@ -3,7 +3,7 @@ mkdir -p $HOME/.ccache/
 echo 'compiler_check = none' >> $HOME/.ccache/ccache.conf
 echo "stats = false" >> $HOME/.ccache/ccache.conf
 echo 'max_size = 20G' >> $HOME/.ccache/ccache.conf
-echo "base_dir = $ROOT/chromium" >> $HOME/.ccache/ccache.conf
+echo "base_dir = $ROOT" >> $HOME/.ccache/ccache.conf
 echo "cache_dir = $ROOT/cache" >> $HOME/.ccache/ccache.conf
 echo "hash_dir = false" >> $HOME/.ccache/ccache.conf
 #git clone --depth=1 https://github.com/AoEiuV020/chromium-ccache-arm64.git $ROOT/cache
