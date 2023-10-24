c9510bfe05a61b2e8ba948e8bdc051b200468a01
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-24 17:41:07 +0800

    Update download_depot.sh

--- script/download_depot.sh
+++ script/download_depot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd "$ROOT"
 git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export DEPOT_TOOLS_DIR=$PWD/depot_tools
 export PATH=$DEPOT_TOOLS_DIR:$PATH
