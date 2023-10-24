9413f01248f5e4079b61c1d8ee06140adbd7cb85
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-24 19:05:52 +0800

    Update gen.sh

--- script/gen.sh
+++ script/gen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd $ROOT/chromium/src
 mkdir -p out/Release64/
-gn gen out/Release64 --args='is_component_build = false is_official_build = true is_debug = false ffmpeg_branding = \"Chrome\" target_cpu = \"x64\" proprietary_codecs = true media_use_ffmpeg = true enable_nacl=false blink_symbol_level=0 v8_symbol_level=0 cc_wrapper="ccache"'
+gn gen out/Release64 --args='is_component_build = false is_official_build = true is_debug = false ffmpeg_branding = \"Chrome\" target_cpu = \"x64\" proprietary_codecs = true media_use_ffmpeg = true enable_nacl=false blink_symbol_level=0 v8_symbol_level=0'
