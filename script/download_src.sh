b6c4552c0001694ab169ae618ce1026135ff53fb
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-26 07:45:03 +0800

    Update download_src.sh

--- script/download_src.sh
+++ script/download_src.sh
@@ -3,7 +3,7 @@ git clone --depth=1 https://github.com/StaZhu/enable-chromium-hevc-hardware-deco
 mkdir $ROOT/chromium && cd $ROOT/chromium
 fetch --nohooks --no-history chromium
 sed -i 's/"custom_vars": {/"custom_vars": {\r\n\t"checkout_pgo_profiles": True,\r\n    /g' .gclient
-sed -i "$a target_os = ['win']" .gclient
+sed -i '$a target_os = ['\''win'\'']' .gclient
 cat $ROOT/chromium/.gclient
 cd $ROOT/chromium/src
 cd third_party/ffmpeg
