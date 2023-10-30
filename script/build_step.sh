0ca08a3fc5fcd35bf62da2c64d7868af879dd937
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-10-30 23:23:30 +0800

    Update build_step.sh

--- script/build_step.sh
+++ script/build_step.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
-test -f "$FLAG_STOP" && echo skip && exit 0 || echo ninja start
-ls -a /usr/local/bin/
+task=$1
+if test ! -n "$task"
+then
+    echo 'usage: build_step.sh <task_name>'
+    exit 1
+fi
+test -f "$FLAG_STOP" && echo skip $task && exit 0 || echo ninja $task
+#ls -a /usr/local/bin/
 #cp /usr/bin/ccache /usr/local/bin/
 #ln -s /usr/local/bin/ccache /usr/local/bin/clang
 #ln -s /usr/local/bin/ccache /usr/local/bin/clang++
 ls -a $HOME/.ccache && cat $HOME/.ccache/ccache.conf
 cd $ROOT/chromium/src
-autoninja -C out/Release64 chrome -j 1 || echo ninja canceld code $?
-COUNT=$(ps -C sleep --no-header |wc -l)
-if [ $COUNT -eq 0 ]
+autoninja -C out/Release64 $task || ( test $? == 143 && echo ninja canceld || echo ninja canceld code $? )
+#autoninja -C out/Release64 chrome -j 1 || echo ninja canceld code $?
+#COUNT=$(ps -C sleep --no-header |wc -l)
+#if [ $COUNT -eq 0 ]
+if true
 then
   cd $ROOT
   git config core.ignorecase false
