23d71fa34cd64ee57560d8d76d762eb8deecb78b
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-11-19 12:28:39 +0800

    Update create_ccache_config.sh

--- script/create_ccache_config.sh
+++ script/create_ccache_config.sh
@@ -5,7 +5,7 @@ mkdir -p $HOME/.ccache/
 echo 'compiler_check = none' >> $HOME/.ccache/ccache.conf
 #echo "stats = false" >> $HOME/.ccache/ccache.conf
 echo 'max_size = 20G' >> $HOME/.ccache/ccache.conf
-echo "base_dir = $ROOT" >> $HOME/.ccache/ccache.conf
+echo "base_dir = $HOME" >> $HOME/.ccache/ccache.conf
 echo "cache_dir = $ROOT/cache" >> $HOME/.ccache/ccache.conf
 echo "hash_dir = false" >> $HOME/.ccache/ccache.conf
 #echo "sloppiness = include_file_mtime" >> $HOME/.ccache/ccache.conf
