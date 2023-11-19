91637824811678cae68f35c220ada1003730ced1
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-11-20 00:31:08 +0800

    Update create_ccache_config.sh

--- script/create_ccache_config.sh
+++ script/create_ccache_config.sh
@@ -2,6 +2,8 @@
 sudo apt-get update
 sudo apt-get install -y ccache
 mkdir -p $HOME/.ccache/
+mkdir -p "$ROOT/cache"
+mkdir -p "$ROOT/debug"
 echo 'compiler_check = none' >> $HOME/.ccache/ccache.conf
 #echo "stats = false" >> $HOME/.ccache/ccache.conf
 echo 'max_size = 20G' >> $HOME/.ccache/ccache.conf
@@ -11,10 +13,8 @@ echo "hash_dir = false" >> $HOME/.ccache/ccache.conf
 #echo "sloppiness = include_file_mtime" >> $HOME/.ccache/ccache.conf
 #echo "sloppiness = time_macros" >> $HOME/.ccache/ccache.conf
 echo "debug = true" >> $HOME/.ccache/ccache.conf
-echo "debug_dir = $ROOT/cache_deg" >> $HOME/.ccache/ccache.conf
-export CCACHE_DEBUGDIR=$ROOT/cache_deg
+echo "debug_dir = $ROOT/debug" >> $HOME/.ccache/ccache.conf
+export CCACHE_DEBUGDIR=$ROOT/debug
 #git clone --depth=1 https://github.com/AoEiuV020/chromium-ccache-arm64.git $ROOT/cache
 ls -a $HOME/.ccache && cat $HOME/.ccache/ccache.conf
-mkdir -p $ROOT/cache/
-mkdir -p $ROOT/cache_deg/
 ls -lh $ROOT
