cbb067a9e3752a31a962f121d4c5ba9567bb2154
yongyesun <68177406+yongyesun@users.noreply.github.com>
2023-11-19 10:38:59 +0800

    Update auto_kill.sh

--- script/auto_kill.sh
+++ script/auto_kill.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sleep $(expr 60 \* 15 \* 3 + ${START_TIME}  - $(date "+%s")) && touch "$FLAG_STOP" && ( killall ninja || true ) || ( test $? == 143 && echo sleep canceld ) &
+sleep $(expr 60 \* 15 \* 2 + ${START_TIME}  - $(date "+%s")) && touch "$FLAG_STOP" && ( killall ninja || true ) || ( test $? == 143 && echo sleep canceld ) &
